aa3bbe236743598d46636747adf11523052af100
Gopal Nambiar <gopuman@Gopals-MacBook-Pro.local>
2020-11-05 19:56:35 +0530

    added nicknames for everyone

--- discord_teams.py
+++ discord_teams.py
@@ -9,6 +9,18 @@ pattern = "\$cleps [0-9]"
 TOKEN = environ["API_KEY"]
 
 n = ['anishkasi', 'Wolfinthehouse', 'pindabc', 'aprbhd', 'the_week', 'JakeSuli', 'gopuman', 'akshara', 'greybeard278', 'Dobby']
+nicknames = {
+        'anishkasi': set(['kasi','anishkasi','kass','kmaams','lebron']),
+        'Wolfinthehouse': set(['neil','pratith','juice','Wolfinthehouse','neilesh','benadryl']),
+        'pindabc': set(['pinds','yogesh','dips','pindabc','upsc']),
+        'aprbhd' : set(['apar','jose','aprbhd','apr']),
+        'the_week' : set(['aveek','tupperware','shelly','the_week','pt','anxx']),
+        'JakeSuli' : set(['arvi','ssr','ronnie','jakesuli']),
+        'gopuman': set(['gops','chilly','gopuman']),
+        'akshara': set(['akshara','hak','omg']),
+        'greybeard278': set(['paro','greybeard278','parz','yawn']), # gops add some funny nickname for me and paro  and others also if you can
+        'Dobby': set(['dobby','kuneil','dpdp','google']),
+        }
 
 weights = {
         'anishkasi': 2.3 + 0.81*3,
@@ -22,6 +34,14 @@ weights = {
         'greybeard278': 1.5 + 0.77*3,
         'Dobby': 1.2 + 0.76*3,
         }
+
+def return_names(split):
+    names = []
+    ss = set(split)
+    for key,val in nicknames.items():
+        if(len(val.intersection(ss)) >= 1):
+            names.append(key)
+    return names
 
 def create_weighted_team(names,num_teams):
     tol = 2
@@ -48,18 +68,22 @@ client = discord.Client(intents=intents)
 def filterOnlyOnlineMembers(member):
     return member.status != discord.Status.offline and not member.bot
 
+def online_members(message):
+    guild = message.guild 
+    vc = guild.voice_channels
+    mems = []
+    for i in vc:
+        members = i.members
+        mems.extend(members)
+    return [i.name for i in mems]
     
 @client.event
 async def on_message(message):
     z = re.findall(pattern,message.content)
     if(len(z) > 0):
-        membersInServer = message.guild.members
-        print(membersInServer)
-        allnames = set([i.name for i in membersInServer])
         split_msg = message.content.split(" ")
-        exclude = list(allnames.intersection(split_msg))
-        onlineMembersInServer = np.array(list(filter(filterOnlyOnlineMembers, membersInServer)))
-        names = [i.name for i in onlineMembersInServer]
+        exclude = return_names(split_msg)
+        names = online_members(message)
         names = [i for i in names if i not in exclude]
         print(names)
         if("-w" in split_msg):
